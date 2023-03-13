1d21bdf04186fb510e0af618f2724a7e7be74d10
Ronald Barasa <barasaogoro@gmail.com>
2023-03-13 08:38:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
+    let input = document.querySelector('input#button');
+
+    function clicked(){
+        console.log("Somebody clicked me!");
+    }
+
+    input.addEventListener('click', clicked)
 }
+ addingEventListener();
+
